becfb0650540cb6077f1b832db7246059376f353
Meadowcottage <bendixon50@gmail.com>
2016-06-04 04:23:23 +0100

    Minified Files
    Finally gotten around to using a minifying tool to auto minify the css files and use them as the primary css stylesheet.

--- app/js/contentscript.js
+++ app/js/contentscript.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 	/** Light Theme Check & Apply **/
 	if ($('link[title=light]').length) {
 		$('link[title=light]').remove();
-		$('head').append( '<link rel="stylesheet" href="' + extensionid + 'css/app-light.css" title="light">' );
+		$('head').append( '<link rel="stylesheet" href="' + extensionid + 'css/app-light.min.css" title="light">' );
 		console.log("[MaterialDeck] Light theme successfully loaded.");
 	} else {
 		console.log("[MaterialDeck] Light theme stylesheet not found. Skipping Overwrite.");
@@ -20,7 +20,7 @@ $(document).ready(function(){
 	/** Dark Theme Check & Apply **/
 	if ($('link[title=dark]').length) {
 		$('link[title=dark]').remove();
-		$('head').append( '<link rel="stylesheet" href="' + extensionid + 'css/app-dark.css" title="dark">' );
+		$('head').append( '<link rel="stylesheet" href="' + extensionid + 'css/app-dark.min.css" title="dark">' );
 		console.log("[MaterialDeck] Dark theme successfully loaded");
 	} else {
 		console.log("[MaterialDeck] Dark theme stylesheet not found. Skipping Overwrite.");
